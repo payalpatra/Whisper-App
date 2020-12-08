475926ff03c5e846aa39fb047ccf8c9b173c573e
payalpatra <payalpatra105@gmail.com>
2020-12-08 13:21:48 +0530

    added favicon

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const findOrCreate = require('mongoose-findorcreate');
 const app = express();
 
 app.use(express.static("public"));
+app.use(favicon(__dirname + '/public/favicon.png'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
