35ee403421f6457a91d8a061fafeb59f858758bc
payalpatra <payalpatra105@gmail.com>
2020-12-08 14:13:55 +0530

    commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+let favicon = require("express-favicon");
 const mongoose = require("mongoose");
 const session = require('express-session');
 const passport = require("passport");
